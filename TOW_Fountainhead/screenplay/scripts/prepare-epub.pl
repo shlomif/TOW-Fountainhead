6c8ee5c0daaa382514a6d3ffedf9794012d56fda
Shlomi Fish <shlomif@shlomifish.org>
2019-08-12 01:31:14 +0300

    avoid recursive make

--- TOW_Fountainhead/screenplay/scripts/prepare-epub.pl
+++ TOW_Fountainhead/screenplay/scripts/prepare-epub.pl
@@ -10,50 +10,47 @@ use Shlomif::Screenplays::EPUB;
 my $gfx = 'towtf-logo-200px.jpg';
 my $obj = Shlomif::Screenplays::EPUB->new(
     {
-        images =>
-        {
+        images => {
             $gfx => "images/$gfx",
         },
     }
 );
 $obj->run;
 
+use Path::Tiny qw/ path /;
 my $filename = $obj->filename;
 
-foreach my $part ($filename =~ /\ATOW_Fountainhead_([0-9]+)/g)
+foreach my $part ( path($filename)->basename =~ /\ATOW_Fountainhead_([0-9]+)/g )
 {
     my $epub_basename = "TOW_Fountainhead_$part";
     $obj->epub_basename($epub_basename);
 
     $obj->output_json(
         {
-            data =>
-            {
+            data => {
                 filename => $epub_basename,
-                title => qq/The One with The Fountainhead - Part $part/,
-                authors =>
-                [
+                title    => qq/The One with The Fountainhead - Part $part/,
+                authors  => [
                     {
                         name => "Shlomi Fish",
                         sort => "Fish, Shlomi",
                     },
                 ],
-                contributors =>
-                [
+                contributors => [
                     {
                         name => "Shlomi Fish",
                         role => "oth",
                     },
                 ],
                 cover => "images/$gfx",
-                rights => "Creative Commons Attribution ShareAlike Unported (CC-by-sa-3.0)",
-                publisher => 'http://www.shlomifish.org/',
-                language => 'en-GB',
-                subjects => [ 'FICTION/Humorous', 'FICTION/Mashups', ],
-                identifier =>
-                {
+                rights =>
+"Creative Commons Attribution ShareAlike Unported (CC-by-sa-3.0)",
+                publisher  => 'http://www.shlomifish.org/',
+                language   => 'en-GB',
+                subjects   => [ 'FICTION/Humorous', 'FICTION/Mashups', ],
+                identifier => {
                     scheme => 'URL',
-                    value => 'http://www.shlomifish.org/humour/TOWTF/',
+                    value  => 'http://www.shlomifish.org/humour/TOWTF/',
                 },
             },
         },
